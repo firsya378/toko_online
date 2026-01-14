d53493514b19a6285e823480b201fd9847e9d81c
firsya378 <firsya_rovalina_33tkj@student.smktelkom-mlg.sch.id>
2026-01-14 13:16:25 +0700

    delete.php

--- delete.php
+++ delete.php
@@ -4,14 +4,14 @@ require 'config.php';
 $id = (int)$_GET['id'];
 
 // cek foto lama
-$res = $conn->query("SELECT foto_peminjaman FROM PEMINJAMAN WHERE id_peminjaman=$id");
+$res = $conn->query("SELECT foto_produk FROM PRODUK WHERE id_produk=$id");
 $data = $res->fetch_assoc();
 
-if ($data && $data['foto_peminjaman'] && file_exists("uploads/" . $data['foto_peminjaman'])) {
-    unlink("uploads/" . $data['foto_peminjaman']);
+if ($data && !empty($data['foto_produk']) && file_exists("uploads/" . $data['foto_produk'])) {
+    unlink("uploads/" . $data['foto_produk']);
 }
 
-$stmt = $conn->prepare("DELETE FROM PEMINJAMAN WHERE id_peminjaman=?");
+$stmt = $conn->prepare("DELETE FROM PRODUK WHERE id_produk=?");
 $stmt->bind_param("i", $id);
 $stmt->execute();
 
